867da09759cb1f3a2cd8791a0291269639af3cac
azu <azuciao@gmail.com>
2012-11-16 15:55:03 +0900

    ひとまず完成

--- SSCheckBoxLayout/SSCheckBoxManager/SSCheckBoxLayoutView.h
+++ SSCheckBoxLayout/SSCheckBoxManager/SSCheckBoxLayoutView.h
@@ -1,31 +1,11 @@
-//
 //  Created by azu on 12/11/16.
 //
 
-
+// ほとんどUIViewなので意味ないけど持っておく
 #import <Foundation/Foundation.h>
 
-typedef NS_ENUM(NSUInteger, SSCheckBoxModeType){
-    MultipleCheckBoxType = 1,
-    SingleCheckBoxType
-};
+@class SSCheckBoxLayout;
 
 @interface SSCheckBoxLayoutView : UIView
-
-// SSCheckBoxViewの配列
-@property(nonatomic, strong) NSMutableArray *checkboxViews;
-
-// ラジオボタンのように単一のチェックするモード
-@property(nonatomic) SSCheckBoxModeType checkBoxMode;// デフォルトは複数選択モード
-
-@property(nonatomic) NSUInteger numberForLines;
-@property(nonatomic) UIEdgeInsets marginForCheckbox;
-
-// Viewの中にチェックボックスを作って入れる
-- (void)makeCheckBoxFromArray:(NSArray *)array InView:(SSCheckBoxLayoutView *)view;
-
-// 配列を渡してUIViewを返すメソッド - 自動レイアウト
-- (UIView *)viewInSize:(CGSize)size addingObjectsFromArray:(NSArray *)array;
-
 
 @end
